9ae0c139f50454d2a6f8bc117b6240b443965036
devitalo <italo_ariel_dev77@outlook.com>
2024-08-03 23:19:38 -0300

    initial commit

---
+++ Estacionamento.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaEstacionamento
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial;
+        private decimal precoHora;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void AdicionarVeiculo(string placa)
+        {
+            if (!string.IsNullOrWhiteSpace(placa))
+            {
+                veiculos.Add(placa);
+                Console.WriteLine($"O veículo com placa {placa} foi adicionado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Placa inválida. Tente novamente.");
+            }
+        }
+
+        public void RemoverVeiculo(string placa, decimal precoInicial, decimal precoHora, decimal qtdHoras)
+        {
+            decimal total = precoInicial + precoHora * qtdHoras;
+
+            if (veiculos.Remove(placa))
+            {
+                Console.WriteLine($"Pague o valor de {total} para que o veículo com placa {placa} seja removido com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Veículo não encontrado.");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Count > 0)
+            {
+                Console.WriteLine("Veículos no estacionamento:");
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum veículo registrado.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SistemaEstacionamento
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Informe o valor inicial:");
+            decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor da hora:");
+            decimal precoHora = Convert.ToDecimal(Console.ReadLine());
+
+            Estacionamento est = new Estacionamento(precoInicial, precoHora);
+
+            string opcao = string.Empty;
+            bool exibirMenu = true;
+
+            // Realiza o loop do menu
+            while (exibirMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua opção:");
+                Console.WriteLine("1 - Cadastrar veículo");
+                Console.WriteLine("2 - Remover veículo");
+                Console.WriteLine("3 - Listar veículos");
+                Console.WriteLine("4 - Encerrar");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Informe a Placa:");
+                        string placa = Console.ReadLine();
+                        est.AdicionarVeiculo(placa);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Informe a Placa do veículo a ser removido:");
+                        string placaRemover = Console.ReadLine();
+                        Console.WriteLine("Informe a quantidade de horas que o veículo permaneceu no estacionamento");
+                        decimal qtdHoras =  Convert.ToDecimal(Console.ReadLine());
+                        est.RemoverVeiculo(placaRemover, precoInicial,precoHora, qtdHoras);
+                        break;
+
+                    case "3":
+                        est.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        exibirMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+
+                // Aguarde o usuário pressionar uma tecla para continuar
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
